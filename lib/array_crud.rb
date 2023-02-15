bd7f34a3431303d3351e7cae3b2a9b892047cf9f
Isaac Ongera <isaacongera717@gmail.com>
2023-02-15 16:09:18 +0300

    completed assignment

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,35 @@
 def create_an_empty_array
+    []
+  end
   
-end
-
-def create_an_array
+  def create_an_array
+    [1, 2, 3, 4]
+  end
   
-end
-
-def add_element_to_end_of_array(array, element)
+  def add_element_to_end_of_array(array, element)
+    array << element
+  end
   
-end
-
-def add_element_to_start_of_array(array, element)
+  def add_element_to_start_of_array(array, element)
+    array.unshift(element)
+  end
   
-end
-
-def remove_element_from_end_of_array(array)
+  def remove_element_from_end_of_array(array)
+    array.pop
+  end
   
-end
-
-def remove_element_from_start_of_array(array)
+  def remove_element_from_start_of_array(array)
+    array.shift
+  end
   
-end
-
-def retrieve_element_from_index(array, index_number)
+  def retrieve_element_from_index(array, index_number)
+    array[index_number]
+  end
   
-end
-
-def retrieve_first_element_from_array(array)
+  def retrieve_first_element_from_array(array)
+    array[0]
+  end
   
-end
-
-def retrieve_last_element_from_array(array)
-  
-end
+  def retrieve_last_element_from_array(array)
+    array[-1]
+  end
